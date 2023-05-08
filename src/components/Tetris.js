752cf37726d971b9b99d2660584ffa49ce10199b
Dao <73803690+daotame@users.noreply.github.com>
2023-05-08 11:38:06 -0400

    Initial commit

---
+++ src/components/Tetris.js
@@ -0,0 +1,37 @@
+import "./Tetris.css";
+import Board from "/src/components/Board";
+import GameStats from "/src/components/GameStats";
+import GameController from "/src/components/GameController";
+import Previews from "/src/components/Previews";
+import { useBoard } from "/src/hooks/useBoard";
+import { useGameStats } from "/src/hooks/useGameStats";
+import { usePlayer } from "/src/hooks/usePlayer";
+
+const Tetris = ({ rows, columns, setGameOver }) => {
+  const [gameStats, addLinesCleared] = useGameStats();
+  const [player, setPlayer, resetPlayer] = usePlayer();
+  const [board, setBoard] = useBoard({
+    rows,
+    columns,
+    player,
+    resetPlayer,
+    addLinesCleared
+  });
+
+  return (
+    <div className="Tetris">
+      <Board board={board} />
+      <GameStats gameStats={gameStats} />
+      <Previews tetrominoes={player.tetrominoes} />
+      <GameController
+        board={board}
+        gameStats={gameStats}
+        player={player}
+        setGameOver={setGameOver}
+        setPlayer={setPlayer}
+      />
+    </div>
+  );
+};
+
+export default Tetris;
